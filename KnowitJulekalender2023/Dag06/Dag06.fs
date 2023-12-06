329f7d4634f2e6a501383263bd6e940425357b38
Terje Wiesener <wiesener@gmail.com>
2023-12-06 16:46:13 +0100

    Knowit dag6

---
+++ KnowitJulekalender2023/Dag06/Dag06.fs
@@ -0,0 +1,27 @@
+﻿open System.IO
+open Utilities.Any
+open Utilities
+
+type State = {
+    Position : float * float
+    DistanceTraveled : float
+}
+
+let moveTo state (x,y) =
+    let (x0,y0) = state.Position
+    let (dx,dy) = (x0 - x, y0 - y)
+    {
+        DistanceTraveled = state.DistanceTraveled + System.Math.Sqrt(dx * dx + dy * dy)
+        Position = (x,y)
+    }
+
+let track = File.ReadAllLines("rute.txt")
+            |> Seq.map (Regex.transformGroups "(-?\d+),(-?\d+)" Value.asFloat)
+            |> Seq.map (fun s -> (Seq.head s, Seq.last s))
+let initial = track |> Seq.head 
+
+track
+|> Seq.skip 1 
+|> Seq.fold moveTo {Position = initial; DistanceTraveled = 0}
+|> (fun state -> ceil (state.DistanceTraveled * 9.0 / 1000.0))
+|> print
