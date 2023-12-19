0a792e76bf116b93d0471b9c9c4230212fc205b2
Terje Wiesener <wiesener@gmail.com>
2023-12-19 09:29:09 +0100

    Day12 part1

---
+++ AdventOfCode2023/Day12/Day12.fs
@@ -0,0 +1,52 @@
+﻿open Utilities
+open System.IO
+open System.Linq
+open FSharp.Collections.ParallelSeq
+//open Day12CS
+
+let fillBlanks str indicesToFill =
+    str |> Seq.mapi (fun i c -> match c with 
+                                | '?' -> if List.contains i indicesToFill then '#' else '.'
+                                | _ -> c )
+        |> Seq.toArray
+        |> System.String
+
+let generateAndTestCombinations str candidates numToPlace isValid =
+    candidates |> SequenceHelper.combinations [] numToPlace
+    |> PSeq.map (fillBlanks str)
+    |> PSeq.filter isValid
+    |> PSeq.length
+
+let countValid str =
+    let groups = str |> Regex.matches "\d+" |> Seq.map (fun m -> (m.Index, int m.Value))
+    let pattern = groups 
+                    |> Seq.map (fun (pos,length) -> length)
+                    |> Seq.map (sprintf "#{%d}")
+                    |> String.concat "\.+"
+    let isValid line = Regex.isMatchPattern pattern line
+
+    let total = groups |> Seq.map (fun (pos,length) -> length) |> Seq.sum
+    let numPlaced = str |> Regex.matches "#" |> Seq.length
+    let candidates = str |> Regex.matches "\?" |> PSeq.map (fun m -> m.Index) |> PSeq.toList
+
+    generateAndTestCombinations str candidates (total - numPlaced) isValid
+
+//File.ReadLines("input.txt")
+//|> Seq.map countValid
+//|> Seq.sum
+//|> printfn "Part 1: %d"
+
+let expandLine str =
+    let parts = str |> String.splitWithAny " "
+    let part1 = Enumerable.Repeat(parts[0], 5) |> String.concat "?"
+    let part2 = Enumerable.Repeat(parts[1], 5) |> String.concat ","
+    part1 + " " + part2
+
+
+
+//File.ReadLines("sample.txt")
+//|> Seq.map expandLine
+//|> Seq.toArray
+//|> Seq.map Day12Solver.CountSolutions
+//|> Seq.sum
+//|> Any.print
--- Utilities/SequenceHelper.fs
+++ Utilities/SequenceHelper.fs
@@ -17,3 +17,52 @@ module SequenceHelper =
         |> Seq.collect (fun (i,r) -> match Set.contains i indicesToDuplicate with
                                         | true -> seq{r; r}
                                         | false -> seq{r} )
+
+
+    //from https://stackoverflow.com/questions/4495597/combinations-and-permutations-in-f
+
+    let rec combinations acc size set = seq {
+      match size, set with 
+      | n, x::xs -> 
+          if n > 0 then yield! combinations (x::acc) (n - 1) xs
+          if n >= 0 then yield! combinations acc n xs 
+      | 0, [] -> yield acc 
+      | _, [] -> () }
+
+    /// Rotates a list by one place forward.
+    let rotate lst =
+        List.tail lst @ [List.head lst]
+
+    /// Gets all rotations of a list.
+    let getRotations lst =
+        let rec getAll lst i = if i = 0 then [] else lst :: (getAll (rotate lst) (i - 1))
+        getAll lst (List.length lst)
+
+    /// Gets all permutations (without repetition) of specified length from a list.
+    let rec getPerms n lst = 
+        match n, lst with
+        | 0, _ -> seq [[]]
+        | _, [] -> seq []
+        | k, _ -> lst |> getRotations |> Seq.collect (fun r -> Seq.map ((@) [List.head r]) (getPerms (k - 1) (List.tail r)))
+
+    /// Gets all permutations (with repetition) of specified length from a list.
+    let rec getPermsWithRep n lst = 
+        match n, lst with
+        | 0, _ -> seq [[]]
+        | _, [] -> seq []
+        | k, _ -> lst |> Seq.collect (fun x -> Seq.map ((@) [x]) (getPermsWithRep (k - 1) lst))
+        // equivalent: | k, _ -> lst |> getRotations |> Seq.collect (fun r -> List.map ((@) [List.head r]) (getPermsWithRep (k - 1) r))
+
+    /// Gets all combinations (without repetition) of specified length from a list.
+    let rec getCombs n lst = 
+        match n, lst with
+        | 0, _ -> seq [[]]
+        | _, [] -> seq []
+        | k, (x :: xs) -> Seq.append (Seq.map ((@) [x]) (getCombs (k - 1) xs)) (getCombs k xs)
+
+    /// Gets all combinations (with repetition) of specified length from a list.
+    let rec getCombsWithRep n lst = 
+        match n, lst with
+        | 0, _ -> seq [[]]
+        | _, [] -> seq []
+        | k, (x :: xs) -> Seq.append (Seq.map ((@) [x]) (getCombsWithRep (k - 1) lst)) (getCombsWithRep k xs)
