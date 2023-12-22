7694f76b271226b03f3ecd7c277473fe533a3f1d
Terje Wiesener <wiesener@gmail.com>
2023-12-22 11:04:52 +0100

    AoC Day21 Part1

---
+++ AdventOfCode2023/Day21/Day21.fs
@@ -0,0 +1,33 @@
+﻿open System.IO
+open Utilities
+
+let expand board =
+    let allOs = board |> Matrix.findAll 'O'
+    let nextSteps = allOs 
+                    |> Seq.map (fun coord -> Matrix.neighborCoordsWithDirection board coord)
+                    |> Seq.collect (fun m -> m.Values)
+                    |> Set.ofSeq
+    board |> Matrix.map (fun m coord ->
+        match (Matrix.get m coord) with
+        | '#' -> '#'
+        | _ -> 
+            match nextSteps.Contains coord with
+            | true -> 'O'
+            | false -> '.'
+    )
+
+let board = File.ReadLines("input.txt") |> Matrix.fromStrings
+
+let start = board |> Matrix.find 'S'
+let boardWithO =  Matrix.withValueAt board start 'O'
+
+seq {1..64}
+|> Seq.fold (fun s a -> expand s) boardWithO
+|> Matrix.findAll 'O'
+|> Seq.length
+|> Any.print
+
+//Idea for part 2: 
+// Find the amount of squares that can be covered in odd or even tiles respectively
+// Find the maximum amount of whole "tiles" that are covered given the max steps
+// Calculate the possible amounts of leftover steps and positions after getting to the edge of the whole tiles, and simulate the remainding part along the edge
